fff58c3d8d6d2c545ec9fee30436e52fb1934ef8
Sam Solano <samsolano@Sams-MacBook-Pro-58.local>
2024-12-04 14:19:54 -0800

    printing and paddle movement

---
+++ Core/Src/UART.h
@@ -0,0 +1,99 @@
+/*
+ * UART.h
+ *
+ *  Created on: Dec 3, 2024
+ *      Author: samsolano
+ */
+
+#ifndef SRC_UART_H_
+#define SRC_UART_H_
+
+#include "stdio.h"
+#include "string.h"
+
+void UART_init(void);
+void USART2_IRQHandler(void);
+void UART_print(char *str);
+void UART_print_int(char *string, uint16_t value);
+void UART_print_float(char *string, float value);
+
+uint8_t paddle_position = 40;
+
+void UART_print_float(char *string, float value) {
+    char buffer[100];
+    int integer_part = (int)value; //separate integer from decimal for easy printing
+    int decimal_part = (int)((value - integer_part) * 1000); // Keep 3 decimal places
+    sprintf(buffer, "%s%d.%03d V\r\n\r\n", string, integer_part, decimal_part);
+    UART_print(buffer);
+}
+
+void UART_print_int(char *string, uint16_t value)
+{
+	char stringify[100] = {0};
+	sprintf(stringify,string ,value); //take value and format it into char array
+	UART_print(stringify);
+}
+
+void UART_init(void)
+{
+	 // Enable clocks for GPIOA and USART2
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;   // Enable GPIOA clock
+	RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN; // Enable USART2 clock
+
+	// Configure PA2 as USART2_TX and PA3 as USART2_RX
+	GPIOA->MODER &= ~(GPIO_MODER_MODE2 | GPIO_MODER_MODE3); // Clear mode bits
+	GPIOA->MODER |= (GPIO_MODER_MODE2_1 | GPIO_MODER_MODE3_1); // Set alternate function mode
+
+	GPIOA->AFR[0] |= (0x7 << GPIO_AFRL_AFSEL2_Pos); // Set AF7 (USART2) for PA2
+	GPIOA->AFR[0] |= (0x7 << GPIO_AFRL_AFSEL3_Pos); // Set AF7 (USART2) for PA3
+
+	// Configure USART2 for 115200 baud rate (assuming 4 MHz clock)
+	USART2->BRR = 208; // Set baud rate divisor for 115200 baud (4 MHz / (16 * 115200) ≈ 35)
+
+	// Enable USART2, transmitter, and receiver and receive data register
+	USART2->CR1 = (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE | USART_CR1_RXNEIE);
+	//enable interrupts
+	NVIC_EnableIRQ(USART2_IRQn);
+}
+
+void UART_print(char *str)
+{
+	while (*str != '\0') 		// Loop until the end of the string
+	{
+		while (!(USART2->ISR & USART_ISR_TXE)) {} //finish receiving
+		USART2->TDR = *str++; 				//write it out
+	}
+
+}
+
+void USART2_IRQHandler(void)
+{
+
+	//if receiving data interrupt
+	if(USART2->ISR & USART_ISR_RXNE)
+	{
+		char single[2];
+		//put input anded with the read register into a variable
+		sprintf(single,"%c", (char)(USART2->RDR & USART_RDR_RDR));
+
+		if(single[0] == 'a') //when s is pressed switch between DC and AC views
+		{
+			if(paddle_position >= 3)
+			{
+				paddle_position -= 3;
+			}
+		}
+		if(single[0] == 'd') //when s is pressed switch between DC and AC views
+		{
+			if(paddle_position <= 76)
+			{
+				paddle_position += 3;
+			}
+		}
+		USART2->ISR &= ~(USART_ISR_RXNE);
+	}
+
+}
+
+
+#endif /* SRC_UART_H_ */
---
+++ Core/Src/main.c
@@ -0,0 +1,206 @@
+#include "main.h"
+#include "UART.h"
+
+#define VIEW_HEIGHT 24
+#define VIEW_WIDTH  80
+#define BALL_X_START 40
+#define BALL_Y_START 10
+
+typedef struct {
+    uint8_t x, y;        // Position
+    uint8_t dx, dy;      // Velocity
+} Ball;
+
+void SystemClock_Config(void);
+void Print_Screen(uint8_t *x_position, uint8_t *y_position);
+void GAME_init(Ball *ball);
+void Check_Collisions(uint8_t *dx, uint8_t *dy, uint8_t *x_position, uint8_t *y_position);
+
+
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+	UART_init();
+
+	Ball ball;
+	GAME_init(&ball);
+
+	while (1)
+	{
+		Print_Screen(&(ball.x), &(ball.y));
+		Check_Collisions(&(ball.dx), &(ball.dy), &(ball.x), &(ball.y));
+	}
+}
+
+
+void Check_Collisions(uint8_t *dx, uint8_t *dy, uint8_t *x_position, uint8_t *y_position)
+{
+	*x_position += *dx;
+	*y_position += *dy;
+
+	if((*x_position == 79) && (*dx > 0))
+	{
+		*dx = -1;
+	}
+	if((*x_position < 3) && (*dx < 0))
+	{
+		*dx = 1;
+	}
+
+	if(*y_position == 1)
+	{
+		*dy = 1;
+	}
+}
+
+
+void Print_Screen(uint8_t *x_position, uint8_t *y_position)
+{
+	for(uint32_t i = 0; i < 500000; i++) {}
+	UART_print("\033[2J\033[H");
+
+	for(uint8_t row = 0; row < VIEW_HEIGHT; row++)
+	{
+
+		for(uint8_t col = 0; col < VIEW_WIDTH; col++)
+		{
+			if((row == *y_position) && (col == *x_position))	// for printing ball
+			{
+				UART_print("o");
+			}
+			if((row == 15) && (col == paddle_position))		// for printing paddle
+			{
+				UART_print("_____");
+				col += 4;
+			}
+			else
+			{
+				UART_print(" ");
+			}
+		}
+		UART_print("\r\n");
+	}
+}
+
+
+void GAME_init(Ball *ball)
+{
+	ball->x = 5;
+	ball->y = BALL_Y_START;
+	ball->dx = -1;
+	ball->dy = -1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_9;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
